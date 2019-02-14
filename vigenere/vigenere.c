eadd1fcbc9ba000c9b27f65fe937d79e15982ae2
maslychm <maslychm@gmail.com>
2019-02-14 03:51:50 -0500

    less global vars

--- vigenere/vigenere.c
+++ vigenere/vigenere.c
@@ -3,79 +3,41 @@
 #include <ctype.h>
 
 FILE *plainFile, *keyFile;
-char c, text[513], keyBuffer[1024], 
-	*key, ciphertext[513];
-
-/* 
-	Open files and return 0 if no error 
-*/
-int openFiles(char *keyf, char* textf)
-{
-	if (!(keyFile = fopen(keyf, "r")))
-	{
-		fprintf(stderr, "Could not open \"%s\"\n", keyf);
-		return -1;
-	}
-	
-	if ( !(plainFile = fopen(textf, "r")) )
-    {
-        fprintf(stderr, "Could not open \"%s\"\n", textf);
-        return -1;
-    }
-	
-	return 0;
-}
+char *key, ciphertext[513];
 
 /*
 	Take only alphabetic chars and make them lower
 */
-int stripInput(FILE * fileptr, int mode)
-{
+int stripInput(FILE * fileptr, char *array, int mode) {
 	int i = 0;
-	int charcnt = 0;
+	char c = 'x';
 	
-	if (mode == 1)
-	{
-		while (fscanf(fileptr,"%c",&c) != EOF && charcnt < 512)
+	while (fscanf(fileptr,"%c",&c) != EOF && i < 512) {
+		if(isalpha(c))
 		{
-			//printf("ye");
-			if(isalpha(c))
-			{
-				//printf("charhere");
-				keyBuffer[i] = tolower(c);
-				i++;
-			}
-			charcnt++;
+			array[i] = tolower(c);
+			i++;
 		}
-		keyBuffer[i] = '\0';
-		key = malloc(sizeof(char) * strlen(keyBuffer) + 1);
-		strcpy(key, keyBuffer);
-		charcnt = 0;
 	}
 	
-	if (mode == 2)
-	{
-		while(fscanf(fileptr,"%c",&c) != EOF && i < 512)
-		{	
-			if(isalpha(c))
-			{
-				//printf("charhere");
-				text[i] = tolower(c);
-				i++;
-			}
-		}
+	if (mode == 1) {
+		array[i] = '\0';
+		key = malloc(sizeof(char) * strlen(array) + 1);
+		strcpy(key, array);
+	}
+	
+	if (mode == 2) {
 		for (i; i < 512; i++)
 		{
-			text[i] = 'x';
+			array[i] = 'x';
 		}
-		text[512] = '\0';
-		//printf("\n\n\nchar %d, i %d",charcnt, i);
+		array[512] = '\0';
 	}
 	
 	return 0;
 }
 
-int encrypt()
+int encrypt(char *text)
 {
 	int i = 0, j = 0;
 	for (i = 0; i < strlen(text); i++)
@@ -90,18 +52,21 @@ int encrypt()
 int main(int argc, char **argv)
 {
 	int i = 0;
+	char keyBuffer[1024], text[513];
 	
-	if (openFiles(argv[1],argv[2]) != 0)
+	if (!(keyFile = fopen(argv[1], "r"))) {
+		fprintf(stderr, "Could not open \"%s\"\n", argv[1]);
 		return -1;
+	}
 	
-	if (stripInput(keyFile, 1) != 0)
-		return -1;
+	if (!(plainFile = fopen(argv[2], "r"))) {
+        fprintf(stderr, "Could not open \"%s\"\n", argv[2]);
+        return -1;
+    }
 	
-	if (stripInput(plainFile, 2) != 0)
-		return -1;
-	
-	if (encrypt() != 0)
-		return -1;
+	stripInput(keyFile, keyBuffer, 1);
+	stripInput(plainFile, text, 2);
+	encrypt(text);
 	
 	printf("\n\nVigenere Key:\n");
 	for (i = 0; i < strlen(key); i++)
