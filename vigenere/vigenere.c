b13764c5110737754ac43ead4b412c8512b01e95
maslychm <maslychm@gmail.com>
2019-02-14 04:09:39 -0500

    even less global vars

--- vigenere/vigenere.c
+++ vigenere/vigenere.c
@@ -3,18 +3,16 @@
 #include <ctype.h>
 
 FILE *plainFile, *keyFile;
-char *key, ciphertext[513];
 
-/*
-	Take only alphabetic chars and make them lower
-*/
-int stripInput(FILE * fileptr, char *array, int mode) {
+char * stripInput(FILE * fileptr, int mode) {
 	int i = 0;
 	char c = 'x';
+	char *array;
+	
+	array = malloc(sizeof(char) * 513);
 	
 	while (fscanf(fileptr,"%c",&c) != EOF && i < 512) {
-		if(isalpha(c))
-		{
+		if(isalpha(c)) {
 			array[i] = tolower(c);
 			i++;
 		}
@@ -22,37 +20,35 @@ int stripInput(FILE * fileptr, char *array, int mode) {
 	
 	if (mode == 1) {
 		array[i] = '\0';
-		key = malloc(sizeof(char) * strlen(array) + 1);
-		strcpy(key, array);
 	}
 	
 	if (mode == 2) {
-		for (i; i < 512; i++)
-		{
+		for (i; i < 512; i++) {
 			array[i] = 'x';
 		}
 		array[512] = '\0';
 	}
 	
-	return 0;
+	return array;
 }
 
-int encrypt(char *text)
-{
-	int i = 0, j = 0;
-	for (i = 0; i < strlen(text); i++)
-	{
+char * encrypt(char *text, char *key) {
+	int i = 0;
+	char *ciphertext;
+	
+	ciphertext = malloc(sizeof(char) * 513);
+	
+	for (i = 0; i < strlen(text); i++) {
 		int c = (text[i] - 97 + key[i % strlen(key)] - 97) % 26  + 97;
 		ciphertext[i] = c;// + 'a';
 	}
 	ciphertext[i] = '\0';
-	return 0;
+	return ciphertext;
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 	int i = 0;
-	char keyBuffer[1024], text[513];
+	char *key, *text, *ciphertext;
 	
 	if (!(keyFile = fopen(argv[1], "r"))) {
 		fprintf(stderr, "Could not open \"%s\"\n", argv[1]);
@@ -64,9 +60,9 @@ int main(int argc, char **argv)
         return -1;
     }
 	
-	stripInput(keyFile, keyBuffer, 1);
-	stripInput(plainFile, text, 2);
-	encrypt(text);
+	key = stripInput(keyFile, 1);
+	text = stripInput(plainFile, 2);
+	ciphertext = encrypt(text, key);
 	
 	printf("\n\nVigenere Key:\n");
 	for (i = 0; i < strlen(key); i++)
